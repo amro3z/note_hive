69e88a8a9383b6e50ab76e3eb5eefe74af25e9d4
Amr Hamouda <amr.hamouda@gmail.com>
2025-07-22 21:04:21 +0300

    add NoteModel

---
+++ lib/models/note_model.dart
@@ -0,0 +1,13 @@
+class NoteModel {
+  final String title;
+  final String description;
+  final DateTime createdTime;
+  final int color;
+
+  NoteModel({
+    required this.title,
+    required this.description,
+    required this.createdTime,
+    required this.color,
+  });
+}
