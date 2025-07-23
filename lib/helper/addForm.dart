8d06788df1f0cf66f40945d7f0af8e929aa5412d
Amr Hamouda <amr.hamouda@gmail.com>
2025-07-23 23:35:01 +0300

    refactor addForm

---
+++ lib/helper/addForm.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+import 'package:note_hive/base/CustomFormTextField.dart';
+import 'package:note_hive/base/customBottom.dart';
+
+// ignore: must_be_immutable
+class AddForm extends StatefulWidget {
+   AddForm({super.key, required this.title, required this.description});
+  String title;
+  String description;
+
+  @override
+  State<AddForm> createState() => _AddFormState();
+}
+
+class _AddFormState extends State<AddForm> {
+   AutovalidateMode autovalidateMode = AutovalidateMode.disabled;
+  GlobalKey<FormState> formKey = GlobalKey<FormState>();
+ 
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.only(
+        bottom: MediaQuery.of(
+          context,
+        ).viewInsets.bottom, // 👈 علشان يتحرك مع الكيبورد
+        left: 16,
+        right: 16,
+        top: 16,
+      ),
+      child: SingleChildScrollView(
+        child: Form(
+          key: formKey,
+          autovalidateMode: autovalidateMode,
+          child: Column(
+            mainAxisSize: MainAxisSize.min, // 👈 علشان يحتفظ بحجمه الطبيعي
+            children: [
+              CustomFormTextField(
+                hintText: 'Title',
+                cursorColor: Colors.blue,
+                enabledBorderColor: Colors.blue,
+                focusedBorderColor: Colors.green,
+                autovalidateMode: autovalidateMode,
+                onSaved: (value) {
+                  widget.title = value ?? '';
+                },
+              ),
+              SizedBox(height: 20),
+              CustomFormTextField(
+                hintText: 'Description',
+                cursorColor: Colors.blue,
+                enabledBorderColor: Colors.blue,
+                focusedBorderColor: Colors.green,
+                maxLines: 5,
+                autovalidateMode: autovalidateMode,
+                onSaved: (value) {
+                   widget.description = value ?? '';
+                },
+              ),
+              SizedBox(height: 20),
+              CustomBottom(
+                title: "Save",
+                onPressed: () {
+                  if (formKey.currentState!.validate()) {
+                    formKey.currentState!.save();
+
+                    Navigator.pop(context);
+                  } else {
+                    autovalidateMode = AutovalidateMode.always;
+                  }
+                },
+              ),
+              SizedBox(height: 24),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/helper/bottomSheet.dart
+++ lib/helper/bottomSheet.dart
@@ -1,74 +1,18 @@
 import 'package:flutter/material.dart';
-import 'package:note_hive/base/CustomFormTextField.dart';
-import 'package:note_hive/base/customBottom.dart';
+import 'package:note_hive/helper/addForm.dart';
 
 class BottomSheetWidget {
+  String title = '';
+  String description = '';
   Future showAddNoteBottomSheet(BuildContext context) {
-    AutovalidateMode autovalidateMode = AutovalidateMode.disabled;
-    GlobalKey<FormState> formKey = GlobalKey<FormState>();
-    String title = '';
-    String description = '';
     return showModalBottomSheet(
       context: context,
       isScrollControlled:
           true, // 👈 ضروري علشان البوتوم شيت يبقى مرن مع الكيبورد
       builder: (context) {
-        return Padding(
-          padding: EdgeInsets.only(
-            bottom: MediaQuery.of(
-              context,
-            ).viewInsets.bottom, // 👈 علشان يتحرك مع الكيبورد
-            left: 16,
-            right: 16,
-            top: 16,
-          ),
-          child: SingleChildScrollView(
-            child: Form(
-              key: formKey,
-              autovalidateMode: autovalidateMode,
-              child: Column(
-                mainAxisSize: MainAxisSize.min, // 👈 علشان يحتفظ بحجمه الطبيعي
-                children: [
-                  CustomFormTextField(
-                    hintText: 'Title',
-                    cursorColor: Colors.blue,
-                    enabledBorderColor: Colors.blue,
-                    focusedBorderColor: Colors.green,
-                    autovalidateMode: autovalidateMode,
-                    onSaved: (value) {
-                      title = value ?? '';
-                    },
-                  ),
-                  SizedBox(height: 20),
-                  CustomFormTextField(
-                    hintText: 'Description',
-                    cursorColor: Colors.blue,
-                    enabledBorderColor: Colors.blue,
-                    focusedBorderColor: Colors.green,
-                    maxLines: 5,
-                    autovalidateMode: autovalidateMode,
-                    onSaved: (value) {
-                      description = value ?? '';
-                    },
-                  ),
-                  SizedBox(height: 20),
-                  CustomBottom(
-                    title: "Save",
-                    onPressed: () {
-                      if (formKey.currentState!.validate()) {
-                        formKey.currentState!.save();
-
-                        Navigator.pop(context);
-                      } else {
-                        autovalidateMode = AutovalidateMode.always;
-                      }
-                    },
-                  ),
-                  SizedBox(height: 24),
-                ],
-              ),
-            ),
-          ),
+        return AddForm (
+          title: title,
+          description: description,
         );
       },
     );
