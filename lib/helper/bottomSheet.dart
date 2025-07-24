85fdb560a033272381040041f1d1abbf8b4b6186
Amr Hamouda <amr.hamouda@gmail.com>
2025-07-24 17:04:34 +0300

    init AddNoteBloc in BottomSheet only tp close it when we don't need it

--- lib/helper/bottomSheet.dart
+++ lib/helper/bottomSheet.dart
@@ -9,45 +9,48 @@ import 'package:note_hive/helper/addForm.dart';
 class BottomSheetWidget {
   String title = '';
   String description = '';
-  Future showAddNoteBottomSheet(BuildContext context) {
-    return showModalBottomSheet(
+  Future showAddNoteBottomSheet(BuildContext context) async {
+    return await showModalBottomSheet(
       context: context,
       isScrollControlled:
           true, // 👈 ضروري علشان البوتوم شيت يبقى مرن مع الكيبورد
       builder: (context) {
-        return Padding(
-          padding: EdgeInsets.only(
-            bottom: MediaQuery.of(
-              context,
-            ).viewInsets.bottom, // 👈 علشان يتحرك مع الكيبورد
-            left: 16,
-            right: 16,
-            top: 16,
-          ),
-          child: BlocConsumer<AddNoteCubit, AddCubitState>(
-            listener: (context, state) {
-              if (state is AddCubitError) {
-                log('Error: ${state.message}');
-              }
-              if (state is AddCubitSuccess) {
-                log('Note added: ${state.title}');
-                Navigator.pop(context);
-              }
-            },
-            builder: (context, state) {
-              return SizedBox(
-                height: MediaQuery.of(context).size.height * 0.35,
-                child: ModalProgressHUD(
-                  color: Colors.transparent,
-                  progressIndicator: CircularProgressIndicator(
-                    color: Colors.green,
+        return BlocProvider(
+          create: (context) => AddNoteCubit(),
+          child: Padding(
+            padding: EdgeInsets.only(
+              bottom: MediaQuery.of(
+                context,
+              ).viewInsets.bottom, // 👈 علشان يتحرك مع الكيبورد
+              left: 16,
+              right: 16,
+              top: 16,
+            ),
+            child: BlocConsumer<AddNoteCubit, AddCubitState>(
+              listener: (context, state) {
+                if (state is AddCubitError) {
+                  log('Error: ${state.message}');
+                }
+                if (state is AddCubitSuccess) {
+                  log('Note added: ${state.title}');
+                  Navigator.pop(context);
+                }
+              },
+              builder: (context, state) {
+                return SizedBox(
+                  height: MediaQuery.of(context).size.height * 0.35,
+                  child: ModalProgressHUD(
+                    color: Colors.transparent,
+                    progressIndicator: CircularProgressIndicator(
+                      color: Colors.green,
+                    ),
+                    blur: 1,
+                    inAsyncCall: state is AddCubitLoading ? true : false,
+                    child: SingleChildScrollView(child: AddForm()),
                   ),
-                  blur: 1 ,
-                  inAsyncCall: state is AddCubitLoading ? true : false,
-                  child: SingleChildScrollView(child: AddForm()),
-                ),
-              );
-            },
+                );
+              },
+            ),
           ),
         );
       },
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hive_flutter/adapters.dart';
-import 'package:note_hive/cubits/addNoteCubit/cubit/add_cubit_cubit.dart';
 import 'package:note_hive/cubits/observer/simple_observer.dart';
 import 'package:note_hive/models/note_model.dart';
 import 'package:note_hive/route.dart';
@@ -19,17 +18,11 @@ class InitWidget extends StatelessWidget {
   final AppRoute appRoute;
   @override
   Widget build(BuildContext context) {
-    return MultiBlocProvider(
-      providers: [
-        BlocProvider(
-          create: (context) => AddNoteCubit(),
-        ),
-      ],
-      child: MaterialApp(
+    return  MaterialApp(
         theme: ThemeData.dark(),
         debugShowCheckedModeBanner: false,
         onGenerateRoute: appRoute.generateRoute,
-      ),
-    );
+      );
+    
   }
 }
